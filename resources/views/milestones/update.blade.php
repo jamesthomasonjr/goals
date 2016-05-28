ce49a739e758f542d9f52a309d5099d2a38f2e5d
James Thomason <james@jamesthomasonjr.com>
2016-05-28 16:52:55 -0400

    Add delete buttons to the edit pages

--- resources/views/milestones/update.blade.php
+++ resources/views/milestones/update.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
 
+<h1>Edit</h1>
 {!! BootForm::open()->action(route('goals.update.action', ['goalId' => $goalId, 'milestoneId' => $milestoneId]))->put() !!}
 {!! BootForm::text('Milestone', 'text')->defaultValue($text) !!}
 {!! BootForm::submit('Update') !!}
+{!! BootForm::close() !!}
+
+<h1>Delete</h1>
+{!! BootForm::open()->action(route('goals.destroy.action', ['goalId' => $goalId, 'milestoneId' => $milestoneId]))->delete() !!}
+{!! BootForm::submit('Delete') !!}
 {!! BootForm::close() !!}
 
 @endsection
--- resources/views/progresses/update.blade.php
+++ resources/views/progresses/update.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
 
+<h1>Edit</h1>
 {!! BootForm::open()->action(route('goals.update.action', ['goalId' => $goalId, 'progressId' => $progressId]))->put() !!}
 {!! BootForm::text('Progress', 'text')->defaultValue($text) !!}
 {!! BootForm::submit('Update') !!}
+{!! BootForm::close() !!}
+
+<h1>Delete</h1>
+{!! BootForm::open()->action(route('goals.destroy.action', ['goalId' => $goalId, 'progressId' => $progressId]))->delete() !!}
+{!! BootForm::submit('Delete') !!}
 {!! BootForm::close() !!}
 
 @endsection
